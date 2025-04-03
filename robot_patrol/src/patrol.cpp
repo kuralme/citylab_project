c6e149975c014737759453d2ee4846adbd763caf
kuralme <kuralme@protonmail.com>
2025-04-03 01:51:33 +0000

    simple obs avoid

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -5,9 +5,14 @@
 class Patrol : public rclcpp::Node {
 public:
   Patrol() : Node("robot_patrol_node"), direction_(0.0) {
+    cmd_vel_pub_ =
+        this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10,
         std::bind(&Patrol::scan_callback, this, std::placeholders::_1));
+
+    move_cmd_.linear.x = 0.0;
+    move_cmd_.angular.z = 0.0;
   }
 
 private:
@@ -20,14 +25,24 @@ private:
 
     // Get laser measurements for the left, right, and forward directions
     this->laser_right = get_min_distance(msg->ranges, 180, 190);
-    this->laser_forward = get_min_distance(msg->ranges, 300, 420);
+    this->laser_forward = get_min_distance(msg->ranges, 300, 460);
     this->laser_left = get_min_distance(msg->ranges, 530, 540);
 
     RCLCPP_INFO(this->get_logger(), "[LEFT] = '%f'", this->laser_left);
     RCLCPP_INFO(this->get_logger(), "[RIGHT] = '%f'", this->laser_right);
     RCLCPP_INFO(this->get_logger(), "[FORWARD] = '%f'", this->laser_forward);
+
+    if (laser_forward > 0.35) {
+      // No obstacle in front, move forward
+      move_cmd_.angular.z = 0.0;
+    } else {
+      move_cmd_.angular.z = .3;
+    }
+    move_cmd_.linear.x = 0.1;
+    cmd_vel_pub_->publish(move_cmd_);
   }
 
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
   geometry_msgs::msg::Twist move_cmd_;
   float laser_left;
