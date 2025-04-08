6b5431386b8e2e88b51f7b00bf7bb6da4df8e88e
kuralme <kuralme@protonmail.com>
2025-04-08 13:34:48 +0000

    direction service added, test client added

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -37,33 +37,11 @@ private:
       move_cmd_.angular.z = 0.0;
     } else {
       // Find the empty space and turn
-      direction_ = find_patrol_direction(msg->ranges);
+      //   direction_ = find_patrol_direction(msg->ranges);
       move_cmd_.angular.z = direction_ / 2;
     }
     move_cmd_.linear.x = 0.1;
     cmd_vel_pub_->publish(move_cmd_);
-  }
-
-  float find_patrol_direction(const std::vector<float> &ranges) {
-    size_t start_idx = 180;
-    size_t end_idx = 540;
-
-    // Find the maximum distance in the range
-    auto max_it =
-        std::max_element(ranges.begin() + start_idx, ranges.begin() + end_idx,
-                         [](float a, float b) { return a > 0.0f && a < b; });
-
-    // Calculate the index of the max element
-    if (max_it != ranges.begin() + end_idx) {
-      size_t max_idx = std::distance(ranges.begin(), max_it);
-
-      // Corresponding angle (deg to rad)
-      int angle_deg = (max_idx - 360) / 2;
-      return angle_deg * M_PI / 180;
-    } else {
-      // No valid max distance found
-      return 0.0;
-    }
   }
 
   float get_min_distance(const std::vector<float> &ranges, size_t start_idx,
@@ -75,17 +53,6 @@ private:
     return (it != ranges.begin() + end_idx)
                ? *it
                : std::numeric_limits<float>::infinity();
-  }
-
-  float get_max_distance(const std::vector<float> &ranges, size_t start_idx,
-                         size_t end_idx) {
-    auto it =
-        std::max_element(ranges.begin() + start_idx, ranges.begin() + end_idx,
-                         [](float a, float b) { return a > 0.0f && a < b; });
-
-    return (it != ranges.begin() + end_idx)
-               ? *it
-               : -std::numeric_limits<float>::infinity();
   }
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
